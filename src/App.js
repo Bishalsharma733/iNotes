55040375ed4d4d26b8bed5b0f7ad4d93d3bb8d27
unknown <bishalsharma733@gmail.com>
2022-04-03 10:22:29 +0545

    nitial

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import About from './components/About';
+import NoteState from './context/notes/NoteState';
+import { Alert } from './components/Alert';
+import Signup from './components/Signup';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert message="This is amazing React course" />
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/login">
+                <Login />
+              </Route>
+              <Route exact path="/signup">
+                <Signup />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
+    </>
+  );
+}
+
+export default App;
